ac9eb26434baa66cadd19af6df3fe21e05bb4e3f
Adi-Ram123 <adi.sr@outlook.com>
2024-03-22 13:13:32 -0500

    Created title screen

--- Frame.java
+++ Frame.java
@@ -1,27 +1,46 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
 public class Frame extends JFrame
 {
+    private JFrame frame;
     public Frame(String frameName)
     {
-        super(frameName);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        pack();
+        //Initial frame setup
+        frame = new JFrame();
+        frame.setTitle(frameName);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1500, 1000);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        
+        frame.setVisible(true);
 
-        Panel p = new Panel();
-        Insets frameInsets = getInsets();
-
-        int frameWidth = p.getWidth() + (frameInsets.left + frameInsets.right);
-        int frameHeight = p.getHeight() + (frameInsets.top + frameInsets.bottom);
-
-        setPreferredSize(new Dimension(frameWidth, frameHeight));
-        setLayout(null);
-
-        add(p);
-        pack();
-
-        setVisible(true);
+        
+        
+        //Display title at start
+        JPanel titlePanel = new JPanel();
+        titlePanel.setBackground(new Color(78, 106, 84));
+        titlePanel.setLayout(null);
+        frame.add(titlePanel);
+        JLabel title = new JLabel("BLACKJACK");
+        titlePanel.add(title);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Arial", Font.PLAIN, 60));
+        title.setBounds(550, 200, 380, 200);
+        // Border blackline = BorderFactory.createLineBorder(Color.black);
+        // title.setBorder(blackline);
+        JButton newRound = new JButton("New Round");
+        titlePanel.add(newRound);
+        newRound.setBounds(625, 400, 200, 100);
+        newRound.setFont(new Font("Arial", Font.PLAIN, 30));
+        newRound.setFocusable(false);
+        
+            
+        
     }
+
+    
 }
